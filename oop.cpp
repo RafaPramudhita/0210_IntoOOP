f9c86cdc83421a981104baa60fd0fa5c1290cba3
RafaPramudhita <161433424+RafaPramudhita@users.noreply.github.com>
2024-04-23 16:09:52 +0700

    menampilkan cara kerjanya dan return

--- oop.cpp
+++ oop.cpp
@@ -38,5 +38,16 @@ class Matakuliah {
 
 
 int main() {
+	Mahasiswa mhs;
+	Matakuliah mk;
+
+	mhs.nama = "Budi";
+	mhs.umur = 20;
+	mhs.jurusan = "Teknik Informatika";
 	
+	mhs.output();
+	mk.input();
+	mk.output();
+
+	return 0;
 }
